cd34d862247585527c309aa1c9b62194acb7b6c7
mkl-74 <mickael1008@gmail.com>
2023-10-13 06:45:38 -0700

    task4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-/*Ecrire une chaine de caracter*/
-
-int main(int argc, char *argv[])
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
 	return (0);
