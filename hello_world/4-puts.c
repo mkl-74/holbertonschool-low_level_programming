87c2d4164bca499f2d1ea03c94b437e56d1f0e80
mkl-74 <mickael1008@gmail.com>
2023-10-13 06:30:23 -0700

    task4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,9 @@
-#!/bin/bash
 #include <stdio.h>
-#include <stdlib.h>
 
-int main ( int argc, char * argv[] ) {
+/*Ecrire une chaine de caracter*/
 
+int main(int argc, char *argv[])
+{
 	puts("Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
