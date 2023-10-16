040adf5dc211d1e36ac59a345021e90187c34762
mkl-74 <mickael1008@gmail.com>
2023-10-16 08:02:50 -0700

    task2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+
+{
+
+	int car;
+
+	for (car = 'a'; car <= 'z'; car++)
+	{
+		putchar(car);
+	}
+		putchar("\n");
+}
