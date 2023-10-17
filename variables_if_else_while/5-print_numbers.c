68b5b26420b6c211f3c47b1b6431992969ee0a5c
mkl-74 <mickael1008@gmail.com>
2023-10-17 01:15:13 -0700

    task5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int nbr;
+
+		for (nbr = 0; nbr <= 9; nbr++)
+{
+
+		printf("%d\n", nbr);
+
+}
+
+
+
+	return (0);
+
+}
