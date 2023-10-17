9a1e60345a9c3427922592632dd7bbab5bc769b9
mkl-74 <mickael1008@gmail.com>
2023-10-17 03:10:05 -0700

    task6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ *
+ */
+int main(void)
+{
+	int debut = 0;
+	int fin = 9;
+	int incr;
+	
+	for (incr = debut; incr <= fin; incr++)
+	{
+		putchar('0'+incr);
+		
+	}
+	putchar('\n');
+
+	return (0);
+}
