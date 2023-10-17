088563782f82a9917f7986da8cdc740532ef74bf
mkl-74 <mickael1008@gmail.com>
2023-10-17 04:27:01 -0700

    task7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowReverse;
+
+	for (lowReverse = 'z'; lowReverse >= 'a'; lowReverse--)
+		putchar (lowReverse);
+{
+	putchar ('\n');
+}
+
+	return (0);
+}
