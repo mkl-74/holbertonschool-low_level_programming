7d7b29d97bd3eb71c426027f98ccdcb89b3ea907
mkl-74 <mickael1008@gmail.com>
2023-10-18 01:44:35 -0700

    task0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include "main.h"
 /**
  * main - Entry point
  *
@@ -9,6 +7,16 @@
 
 int main(void)
 
-	printf("\_putchar");
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+
 
 	return (0);
+}
---
+++ functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+	
+int _putchar(char c);
+	/*ajouter tout les prototype qui ne sont que l'image de nos function*/
+
+
+#endif
