cd4d61357a07fde393b8fa18c4b4341b9436ba6d
mkl-74 <mickael1008@gmail.com>
2023-10-18 02:05:18 -0700

    task0

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,8 +1,8 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-	
-int _putchar(char c);
-	/*ajouter tout les prototype qui ne sont que l'image de nos function*/
+#ifndef Main_h
+#define Main_h
 
+int _putchar(char c);
+
+/*ecris la function*/
 
 #endif
