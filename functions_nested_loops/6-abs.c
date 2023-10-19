79a09557a111cd41e76de06b8b0685ed07f283f4
mkl-74 <mickael1008@gmail.com>
2023-10-19 06:52:48 -0700

    task6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  _abs(int) - return the absolute value of the number
+ * @n:
+ *
+ * Return: Absolute number
+ *
+ */
+int _abs(int)
+{
+	int nbr;
+
+	if (nbr < 0)
+	{
+		nbr = -nbr;
+	}
+		printf("%d\n", nbr);
+
+	else
+		printf("%d\n", nbr)
+
+
+	return (0);
+}
