7a31e2d2b8e93b0ed8c104ada005abb787d6061e
mkl-74 <mickael1008@gmail.com>
2023-10-19 10:47:13 -0700

    task7

--- functions_nested_loops/7-main.c
+++ functions_nested_loops/7-main.c
@@ -1,15 +1,18 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code.
- *
+ * main - check the code
  *
  * Return: Always 0.
- *
  */
 int main(void)
 {
-	int print_last_digit(int);
+	int r;
+
+	print_last_digit(98);
+	print_last_digit(0);
+	r = print_last_digit(-1024);
+	_putchar('0' + r);
+	_putchar('\n');
 	return (0);
 }
