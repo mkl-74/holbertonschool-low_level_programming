bdb1aada3102821832676df04581ce1573ace528
mkl-74 <mickael1008@gmail.com>
2023-10-19 07:47:43 -0700

    task7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _print_last_digit - function that display the last digit of a number
+ * @nbr:  is the int that will use for the argument of the function
+ * Return: 0
+ */
+int print_last_digit(int nbr);
+{
+	int nbr;
+	int last;
+
+	last = nbr % 10;
+
+	printf("%d\n", last)
+}
