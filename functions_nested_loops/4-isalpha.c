13cdac1245c3782446c119e7df753b70ef0e2a32
mkl-74 <mickael1008@gmail.com>
2023-10-19 04:44:30 -0700

    task4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - permet de tester si un caractère est alphabétique ou non.
+ * @c:
+ *
+ * Return: 1 if the alphabet charactere. 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' && c >= 'A' &&  c <= 'Z')
+	{
+		return (1);
+	}
+		return (0);
+}
---
+++ functions_nested_loops/4-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ *
+ * Return: Always O.
+ */
+int main(void)
+{
+	int _isalpha(int c);
+	return (0);
+}
