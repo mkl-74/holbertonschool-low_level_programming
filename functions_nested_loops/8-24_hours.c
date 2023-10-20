846656f4fe13405df4242b323b8471c493c30fe2
mkl-74 <mickael1008@gmail.com>
2023-10-20 00:43:51 -0700

    task8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <time.h>
 
 /**
  * jack_bauer - that prints every minute of the day of Jack Bauer.
@@ -13,15 +12,17 @@ void jack_bauer(void)
 	int heures;
 	int minutes;
 
-	for (heures = 0; heures <= 59; heures++)
+	for (heures = 0; heures <= 23; heures++)
 	{
 		for (minutes = 0; minutes <= 59; minutes++)
 		{
-				_putchar (heures / 10 + '0');
-				_putchar (heures % 10 + '0');
-				_putchar (':');
-				_putchar (minutes / 10 + '0');
-				_putchar (minutes / 10 + '0');
+				_putchar((heures / 10) + '0');
+				_putchar((heures % 10) + '0');
+				_putchar(':');
+				_putchar((minutes / 10) + '0');
+				_putchar((minutes % 10) + '0');
+
+				_putchar('\n');
 		}
 
 	}
