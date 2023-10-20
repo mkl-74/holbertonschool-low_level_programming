97e90ca3ee282624057659ee04b6500fda9ae2e7
mkl-74 <mickael1008@gmail.com>
2023-10-20 05:43:01 -0700

    task11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void print_to_98(int n)
 		while (n >= 98)
 		{
 			printf("%d, ", n--);
-			printf("%d", n);
+			/*printf("%d", n);*/
 		}
 	}
 	else
@@ -22,7 +22,7 @@ void print_to_98(int n)
 		while (n <= 98)
 		{
 			printf("%d, ", n++);
-			printf("%d\n", n);
+			/*printf("%d\n", n);*/
 		}
 	}
 }
