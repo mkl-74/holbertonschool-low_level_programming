6f31aaf2e936adbbf0f2876f866770fb00aef6f2
mkl-74 <mickael1008@gmail.com>
2023-10-20 00:36:05 -0700

    task8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -16,17 +16,14 @@ void jack_bauer(void)
 	for (heures = 0; heures <= 59; heures++)
 	{
 		for (minutes = 0; minutes <= 59; minutes++)
-
-			{
+		{
 				_putchar (heures / 10 + '0');
 				_putchar (heures % 10 + '0');
-			}
-			{
 				_putchar (':');
-			}
-			{
 				_putchar (minutes / 10 + '0');
 				_putchar (minutes / 10 + '0');
-			}
+		}
+
 	}
+
 }
