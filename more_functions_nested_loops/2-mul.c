d503bf3cc1e990a9a653bc357c84ed51d7c39c70
mkl-74 <mickael1008@gmail.com>
2023-10-22 09:44:37 -0700

    task2

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,16 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-* _mul(int a, int b) - write a function that multiple two integer.
+* mul - (int a, int b) - write a function that multiple two integer.
 * @a: integer
 * @b: integer
-* @sum: result
 * Return: multiple a * b.
-**/
+*/
 int mul(int a, int b)
 {
-	sum = a * b;
-
-	printf("%d\n", sum);
+	return (a * b);
 }
