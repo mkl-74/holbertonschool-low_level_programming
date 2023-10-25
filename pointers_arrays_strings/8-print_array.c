23855458a7f61c6efb1915675ec9fd5112aec51d
mkl-74 <mickael1008@gmail.com>
2023-10-25 05:22:10 -0700

    task8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - display n elements of array
+ * @a: ptr
+ * @n:
+ *
+ *
+ *
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+	printf("%d", a[i]);
+
+	if (i < n - 1)
+	{
+	printf("; ");
+	}
+}
+
+	printf("\n");
+}
