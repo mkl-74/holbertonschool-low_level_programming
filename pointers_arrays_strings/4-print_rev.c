b3c595181bfacccfdae2c876e2f933677e17a113
mkl-74 <mickael1008@gmail.com>
2023-10-25 00:24:14 -0700

    task4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,10 +9,17 @@
  */
 void print_rev(char *s)
 {
+	int length = 0;
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[length] != '\0')
 	{
-			_putchar(s[i]);
+	length++;
 	}
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+		_putchar('\n');
 }
