f6044925f4a86f128bdb5c11e909868a42d1fafb
mkl-74 <mickael1008@gmail.com>
2023-10-27 06:47:25 -0700

    task 5 string toupper

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -7,13 +7,13 @@
 
 char *string_toupper(char *s)
 {
-	int count = 0;
+	int i = 0;
 
-	while (*(s + count) != '\0')
+	while (*(s + i) != '\0')
 	{
-		if ((*(s + count) >= 97) && (*(s + count) <= 122))
-			*(s + count) = *(s + count) - 32;
-		count++;
+		if ((*(s + i) >= 97) && (*(s + i) <= 122))
+			*(s + i) = *(s + i) - 32;
+		i++;
 	}
 
 	return (s);
