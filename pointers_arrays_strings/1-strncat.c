875b7a337065af29c9621302e67b45f550ad7e5b
mkl-74 <mickael1008@gmail.com>
2023-10-27 00:52:16 -0700

    task 1 strncat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,27 @@
 #include "main.h"
 
 /**
+ * _strncat - add src to end dest and overwrite '\0'
+ * @dest: 
+ * @src:
  *
- *
- *
- *
- *
+ *Return: dest
  *
  */
+char *_strncat(char *dest, char *src, int n)
+{
+	while (*dest != '\0') {
+	dest++;
+	}
+
+	while (*src != '\0' && n > 0) {
+	*dest = *src;
+	dest++;
+	src++;
+	n--;
+	}
+	*dest = '\0';
+
+	return (dest);
+}
 
