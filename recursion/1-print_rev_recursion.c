2f8621f0af02d293b8c1dd26affdf6546829ab24
mkl-74 <mickael1008@gmail.com>
2023-10-30 06:37:31 -0700

    task 1 rev_recursion

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print a string in reverse
+ * @s: ptr
+ *
+ *
+ * Return: s + 1
+ *
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
