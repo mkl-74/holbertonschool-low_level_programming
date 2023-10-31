db1eaded58dd7354a0ac9a1bf06ca85f178e4368
mkl-74 <mickael1008@gmail.com>
2023-10-31 02:26:39 -0700

    task 4 pow_recursion

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (x == 0)
+	{
+		return (1);
+	}
+	else
+	
+		return (_pow_recursion(x, y - 1) * x);
+	
+}
