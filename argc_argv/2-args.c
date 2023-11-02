053893783904cf80f52d9c1696e741bfc7882918
mkl-74 <mickael1008@gmail.com>
2023-11-02 06:54:09 -0700

    task 2

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 
 /**
+ * main - print all arguments
+ *@argc: value of argument
+ *@argv: numbre of argument
  *
  *
- *
- *
- *
- *
+ * Return: 0
  */
-	
 int main(int argc, char *argv[])
 {
 	int i = 0;
 
-    for (i = 0; i < argc; i++)
-    {
-        printf("%s\n", argv[i]);
-    }
+	for (i = 0; i < argc; i++)
+	{
+	printf("%s\n", argv[i]);
+	}
 
-    return 0;
+	return (0);
 }
