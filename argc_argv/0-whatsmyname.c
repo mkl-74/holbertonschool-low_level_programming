092418c4bb9f9a0bf52339bfbbf3864367e4fc42
mkl-74 <mickael1008@gmail.com>
2023-11-02 05:32:17 -0700

    task0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * @argc: argument count
+ * @argv: array argument
+ *
+ * Return 0.
+ *
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+
+	}
+	return (0);
+
+
+}
