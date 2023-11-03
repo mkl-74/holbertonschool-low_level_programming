99e768e8454504fc05940ee66f84c935a05a6116
mkl-74 <mickael1008@gmail.com>
2023-11-03 01:37:59 -0700

    task 3 mul

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -3,27 +3,26 @@
 #include <stdlib.h>
 /**
  * main - Multiply 2 integer
- * @nbr1:
- * @nbr2:
- * @result:
+ * @argc: number of element command line
+ * @argv: content element in tha array
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	
+
 	int nbr1;
 	int nbr2;
 	int result;
 
 	if (argc < 3)
 	{
-	printf("error");
+	printf("error\n");
 		return (1);
 	}
 
 	nbr1 = atoi(argv[1]);
-        nbr2 = atoi(argv[2]);
-        result = nbr1 * nbr2;
+	nbr2 = atoi(argv[2]);
+	result = nbr1 * nbr2;
 
 		printf("%d\n", result);
 	return (0);
