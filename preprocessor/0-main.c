4b4fcdb58434439ca9a67857f7d5704c895b98b6
mkl-74 <mickael1008@gmail.com>
2023-11-10 03:22:09 -0800

    task0

--- preprocessor/0-main.c
+++ preprocessor/0-main.c
@@ -1,4 +1,5 @@
-#define SIZE 1024
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
 #include <stdio.h>
 
 /**
@@ -14,4 +15,3 @@ int main(void)
     printf("%d\n", s);
     return (0);
 }
-
