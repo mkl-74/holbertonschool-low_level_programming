694d17a2c38d93c4cf9fe29a531deb1e89768c95
mkl-74 <mickael1008@gmail.com>
2023-11-13 03:03:33 -0800

    task0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - structure dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: other data
+ * return - dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
