1a2855024461b2e70623aebffd6888eeeb2132cf
mkl-74 <mickael1008@gmail.com>
2023-11-17 15:03:17 +0000

    task2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -2,11 +2,10 @@
 #include <stdarg.h>
 
 /**
-*
-*
-*
-*
-*
+* print_strings - print a strings following a separator
+* @separator: print separator between number
+* @n: number of string in the function
+* @...: list with integer to print
 */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
@@ -23,13 +22,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(args, char*);
-	    if (i < n-1 && separator != NULL)
+	if (i < n - 1 && separator != NULL)
 		{
-			printf("%s", str);
-			printf("%s", separator);
+		printf("%s", str);
+		printf("%s", separator);
 		}
-        else if (str == NULL)
-                printf("(nil)");
+		else if (str == NULL)
+			printf("(nil)");
 		else
 			printf("%s", str);
 	}
