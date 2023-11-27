dd00460888604f5f3809727520764aae60a2db85
mkl-74 <mickael1008@gmail.com>
2023-11-27 13:18:36 +0000

    0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -4,10 +4,30 @@
 #include "lists.h"
 
 /**
-*
+* print_list -
 *
 *
 *
 *
 */
-size_t print_list(const list_t *h);
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *current = h;
+
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+        {
+			printf("[0] (nil)\n");
+        }
+		else
+			printf("%s\n", current->str);
+
+		current = current->next;
+		count++;
+	}
+
+
+	return (count);
+}
