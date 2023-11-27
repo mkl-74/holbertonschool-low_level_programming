d161829f0a2605b2d1f7a53448c583ff523efe5f
mkl-74 <mickael1008@gmail.com>
2023-11-27 15:13:56 +0000

    lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,9 +1,21 @@
-#ifndef LIST_H
-#define LIST_H
-#include <stdarg.h>
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+typedef struct list_s
+{
+	char *str;
+
+	unsigned int len;
+
+	struct list_s *next;
+} list_t;
+
 
 /*Prototypes*/
 
-int _putchar(char c);
-
 size_t print_list(const list_t *h);
+
+
+#endif
