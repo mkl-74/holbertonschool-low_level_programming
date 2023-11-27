f43c99709bd549db093fd12f8ce4bb0a2758bd86
mkl-74 <mickael1008@gmail.com>
2023-11-27 12:43:20 +0000

    putchar push

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "lists.h"
+
+/**
+*
+*
+*
+*
+*
+*/
+size_t print_list(const list_t *h);
---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,8 @@
+#ifndef LIST_H
+#define LIST_H
+#include <stdarg.h>
+
+/*Prototypes*/
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
