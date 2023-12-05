bf2d4970508af28fdec6dce78a61903a4b75f5bc
mkl-74 <mickael1008@gmail.com>
2023-12-05 09:31:17 +0000

    task0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,20 +1,48 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
- * print_dlistint - print element in list
- * @h:struct list
- * Return:list len
+ * _strlen - lenght of string
+ * @s:char
+ * Return:int
  */
-size_t print_dlistint(const dlistint_t *h)
+int _strlen(const char *s)
 {
 	int i;
-	size_t len = 0;
 
-	for (i = 0; h; i++)
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			continue;
+		}
+return (i);
+}
+/**
+ * binary_to_uint - convert binary to unsigned int
+ * @b:string from 0 and 1 else NULL
+ *
+ * Return:unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int k = 1;
+	unsigned int i = 0;
+	int c;
+	unsigned int len;
+
+	if (b == NULL)
+		return (0);
+
+	len = _strlen(b);
+
+	for (c = len - 1; c >= 0; c--)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
-		len++;
+	if (b[c] != '0' && b[c] != '1')
+		return (0);
+	if (b[c] == '1')
+	{
+		i += k;
 	}
-return (len);
+	k *= 2;
+}
+	return (i);
 }
