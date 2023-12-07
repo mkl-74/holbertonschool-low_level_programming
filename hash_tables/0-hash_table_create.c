f87ab8c236aaee804facf1d10e4e6d436638a1c1
mkl-74 <mickael1008@gmail.com>
2023-12-07 13:58:53 +0000

    task0

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,14 @@
+
+
+int hachage(char *chaine)
+{
+    int i = 0, nombreHache = 0;
+
+    for (i = 0 ; chaine[i] != '\0' ; i++)
+    {
+        nombreHache += chaine[i];
+    }
+    nombreHache %= 100;
+
+    return nombreHache;
+}
