2f85a48966889e4f229e76b2964aa35d0ffe0d78
mkl-74 <mickael1008@gmail.com>
2023-12-08 09:45:48 +0000

    task1

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -15,7 +15,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
+
 	hash_table->size = size;
+
 	hash_table->array = malloc(sizeof(hash_table->array) * size);
 	if (hash_table->array == NULL)
 		return (NULL);
