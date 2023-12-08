bd684682b56ecf3acffe4e5a892c47aee17db5fa
mkl-74 <mickael1008@gmail.com>
2023-12-08 14:30:28 +0000

    task2

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,48 +1,16 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "hash_tables.h"
-
-
 /**
- * hash_table_set - add elements to hash table
- * @ht: hash table
- * @key: key
- * @value: value associeted to the key to add
- * Return: 0(Fails) 1(Succes)
+ * key_index - gives you the index of a key
+ * @key: is the key
+ * @size: size of the array of hash table
+ * Return: index of the key
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int index, size;
-	hash_node_t *node, *temp;
+	unsigned long int hash, index;
 
-	if (!key || !value || !ht)
-		return (0);
-	size = ht->size;
-	index = key_index((const unsigned char *)key, size);
-	temp = ht->array[index];
-	while (temp)
-	{
-		if (temp && strcmp(key, temp->key) == 0)
-		{
-			free(temp->value);
-			temp->value = strdup(value);
-			if (temp->value == NULL)
-				return (0);
-			return (1);
-		}
-		temp = temp->next;
-	}
-	node = malloc(sizeof(hash_node_t));
-	if (node == NULL)
-		return (0);
-	node->key = strdup(key);
-	if (node->key == NULL)
-		return (0);
-	node->value = strdup(value);
-	if (node->value == NULL)
-		return (0);
-	node->next = ht->array[index];
-	ht->array[index] = node;
-	return (1);
+	hash = hash_djb2(key);
+	index = hash % size;
+
+	return (index);
 }
