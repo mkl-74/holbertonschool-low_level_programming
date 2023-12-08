66fbd5c41647b4b16e5801e8b36a572d3310e8ab
mkl-74 <mickael1008@gmail.com>
2023-12-08 17:12:43 +0000

    task6

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,40 +1,30 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "hash_tables.h"
 
 /**
- * main - check the code
- *
- * Return: Always EXIT_SUCCESS.
+ * hash_table_delete - delete a hash table
+ * @ht:hash table
+ * Return: void
  */
-int main(void)
+void hash_table_delete(hash_table_t *ht)
 {
-    hash_table_t *ht;
-    char *key;
-    char *value;
+	unsigned long int i;
+	hash_node_t *node;
 
-    ht = hash_table_create(1024);
-    hash_table_set(ht, "c", "fun");
-    hash_table_set(ht, "python", "awesome");
-    hash_table_set(ht, "Bob", "and Kris love asm");
-    hash_table_set(ht, "N", "queens");
-    hash_table_set(ht, "Asterix", "Obelix");
-    hash_table_set(ht, "Betty", "Cool");
-    hash_table_set(ht, "98", "Battery Streetz");
-    key = strdup("Tim");
-    value = strdup("Britton");
-    hash_table_set(ht, key, value);
-    key[0] = '\0';
-    value[0] = '\0';
-    free(key);
-    free(value);
-    hash_table_set(ht, "98", "Battery Street");
-    hash_table_set(ht, "hetairas", "Bob");
-    hash_table_set(ht, "hetairas", "Bob Z");
-    hash_table_set(ht, "mentioner", "Bob");
-    hash_table_set(ht, "hetairas", "Bob Z Chu");
-    hash_table_print(ht);
-    hash_table_delete(ht);
-    return (EXIT_SUCCESS);
+	if (!ht || !ht->array || !ht->size)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		while (ht->array[i])
+		{
+			node = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = node;
+		}
+	}
+	free(ht->array);
+	ht->array = NULL;
+	ht->size = 0;
+	free(ht);
 }
