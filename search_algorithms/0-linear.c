e3adaa7ee3ad1ec32f01f6eaa6ed3b9788745e71
mkl-74 <mickael1008@gmail.com>
2024-01-17 10:50:53 +0000

    task0

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Recherche linéaire dans un tableau
+ * @array: Pointeur vers le tableau
+ * @size: Taille du tableau
+ * @value: Valeur à rechercher dans le tableau
+ *
+ * Return: L'index de la première occurrence de la valeur dans le tableau,
+ *         ou -1 si la valeur n'est pas présente.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+        size_t i;
+
+    if (array == NULL)
+        return -1;
+
+
+    for (i = 0; i < size; i++)
+    {
+        if (array[i] == value)
+            return (i);
+    }
+
+    return -1;
+}
