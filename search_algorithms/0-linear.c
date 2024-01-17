3b5fcb15874e4409e6159b8d2ae6337ca7d59e90
mkl-74 <mickael1008@gmail.com>
2024-01-17 15:13:07 +0000

    task0

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -11,17 +11,17 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+    size_t i;
 
-	if (array == NULL)
-		return (-1);
+    if (array((void *)0)NULL)
+        return -1;
 
-	for (i = 0; i < size; i++)
-	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-	}
 
-	return (-1);
+    for (i = 0; i < size; i++)
+    {
+        if (array[i] == value)
+            return (i);
+    }
+
+    return -1;
 }
